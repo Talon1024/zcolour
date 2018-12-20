376d38382fa91665c44e2ed317256914b8eb48ec
Kevin Caccamo <kevin@ciinet.org>
2018-12-20 18:14:38 -0500

    Deploy static version of ZColour to GitHub pages

---
+++ precache-manifest.515765404c4f2c5c81c928b2a8fe382c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b829e72534760da90a69",
+    "url": "/static/js/main.b829e725.chunk.js"
+  },
+  {
+    "revision": "bcced97f19293d9b0387",
+    "url": "/static/js/1.bcced97f.chunk.js"
+  },
+  {
+    "revision": "b829e72534760da90a69",
+    "url": "/static/css/main.78bfec44.chunk.css"
+  },
+  {
+    "revision": "00d0e80d2afb49d542b0e8ee17b52108",
+    "url": "/index.html"
+  }
+];
